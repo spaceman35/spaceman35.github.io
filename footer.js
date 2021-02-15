172aa1966b190c1e7cc6204e6358a9456757bcf7
Sean Kelly <seanrkelly35@gmail.com>
2021-02-15 16:05:38 -0500

    Update footer.js

--- footer.js
+++ footer.js
@@ -5,7 +5,7 @@ class Footer extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
         <footer>
-        <p>Contact me via <a href="mailto:seanrkelly35@gmail.com" target="_blank" rel="noopener noreferrer">e-mail</a> [seanrkelly35 AT gmail DOT com]</p>
+        <p>Contact me via <a href="mailto:hello@seankelly.space" target="_blank" rel="noopener noreferrer">e-mail</a> [hello AT seankelly DOT space]</p>
         <p>Find me on <a href="https://www.instagram.com/srkellyscope/" target="_blank" rel="noopener noreferrer">Instagram</a></p>
       </footer>
     `;
